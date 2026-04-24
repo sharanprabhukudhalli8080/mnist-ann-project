52c73e5228594bfc3150b354738e5c9803b47f30
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-04-24 23:35:16 +0530

    Add prediction code for MNIST digit recognition
    Implement prediction functionality for MNIST model using TensorFlow.

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,28 @@
-PASTE YOUR PREDICT CODE HERE
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load model
+model = tf.keras.models.load_model("../models/mnist_model.h5")
+
+# Load test data
+test_data = pd.read_csv("mnist_test.csv")
+
+x_test = test_data.iloc[:, 1:].values / 255.0
+y_test = test_data.iloc[:, 0].values
+
+x_test = x_test.reshape(-1, 28, 28)
+
+# Pick sample
+index = 20
+image = x_test[index]
+
+# Predict
+prediction = model.predict(image.reshape(1,28,28))
+digit = np.argmax(prediction)
+
+# Show result
+plt.imshow(image, cmap='gray')
+plt.title(f"Predicted: {digit}, Actual: {y_test[index]}")
+plt.show()
