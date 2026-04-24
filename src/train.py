a238d88479defe85184ae174dce2f14857e8910c
Sharanprabhu Kudhalli <sharanprabhukudhalli80@gmail.com>
2026-04-24 23:36:07 +0530

    Implement training script for MNIST dataset

--- src/train.py
+++ src/train.py
@@ -1,1 +1,46 @@
-PASTE YOUR TRAIN CODE HERE
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+# Load data
+train_data = pd.read_csv("mnist_train.csv")
+test_data = pd.read_csv("mnist_test.csv")
+
+# Split
+x_train = train_data.iloc[:, 1:].values
+y_train = train_data.iloc[:, 0].values
+
+x_test = test_data.iloc[:, 1:].values
+y_test = test_data.iloc[:, 0].values
+
+# Normalize
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Reshape
+x_train = x_train.reshape(-1, 28, 28)
+x_test = x_test.reshape(-1, 28, 28)
+
+# Model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate
+loss, acc = model.evaluate(x_test, y_test)
+print("Accuracy:", acc)
+
+# Save
+model.save("../models/mnist_model.h5")
